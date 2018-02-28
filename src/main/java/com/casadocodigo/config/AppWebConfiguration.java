9880f1efa2267bc4d6d2ba17103b685aabbca51e
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2018-02-27 21:38:36 -0300

    Respondendo mais de um formato de dados

--- src/main/java/com/casadocodigo/config/AppWebConfiguration.java
+++ src/main/java/com/casadocodigo/config/AppWebConfiguration.java
@@ -1,5 +1,7 @@
 package com.casadocodigo.config;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 import org.springframework.cache.CacheManager;
@@ -13,12 +15,16 @@ import org.springframework.format.datetime.DateFormatter;
 import org.springframework.format.datetime.DateFormatterRegistrar;
 import org.springframework.format.support.DefaultFormattingConversionService;
 import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.accept.ContentNegotiationManager;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.ContentNegotiatingViewResolver;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.casadocodigo.config.resolver.JsonViewResolver;
 import com.casadocodigo.controller.HomeController;
 import com.google.common.cache.CacheBuilder;
 
@@ -76,6 +82,18 @@ public class AppWebConfiguration {
 		GuavaCacheManager manager = new GuavaCacheManager();
 		manager.setCacheBuilder(builder);
 		return manager;
+	}
+	
+	@Bean
+	public ViewResolver contentNegotiatingViewResolver(ContentNegotiationManager manager) {
+		List<ViewResolver> resolvers = new ArrayList<>();
+		resolvers.add(internalResourceViewResolver());
+		resolvers.add(new JsonViewResolver());
+		
+		ContentNegotiatingViewResolver resolver = new ContentNegotiatingViewResolver();
+		resolver.setViewResolvers(resolvers);
+		resolver.setContentNegotiationManager(manager);
+		return resolver;
 	}
 
 }
