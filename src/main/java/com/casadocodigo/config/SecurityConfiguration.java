65a7d74f112a3b85643deb92ffbca68b89c522fa
Rafael Monteiro <rafaelmonteiro.developer@gmail.com>
2018-03-03 22:18:06 -0300

    Customizando regras de acesso

--- src/main/java/com/casadocodigo/config/SecurityConfiguration.java
+++ src/main/java/com/casadocodigo/config/SecurityConfiguration.java
@@ -1,9 +1,26 @@
 package com.casadocodigo.config;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		
+		http.authorizeRequests()
+			.antMatchers("/produtos/form").hasAnyRole("ADMIN")
+			.antMatchers("/carrinho/**").permitAll()
+			.antMatchers(HttpMethod.POST, "/produtos").hasAnyRole("ADMIN")
+			.antMatchers(HttpMethod.GET,  "/produtos").hasAnyRole("ADMIN")
+			.antMatchers("/produtos/**").permitAll()
+			.antMatchers("/home").permitAll()
+			.anyRequest().authenticated()
+		.and()
+			.formLogin();
+	}
+	
 }
